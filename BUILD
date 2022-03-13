7cc36618a593b550cedea1e4ec7076e1cede9cad
Brandon Duffany <brandon@buildbuddy.io>
2022-03-13 19:45:17 -0400

    Initial commit

---
+++ BUILD
@@ -0,0 +1,5 @@
+sh_test(
+    name = "sleepy_test",
+    srcs = ["sleepy_test.sh"],
+)
+
---
+++ sleepy_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+for i in $(seq 10); do
+  echo "Sleeping"
+  sleep 1
+done
+
